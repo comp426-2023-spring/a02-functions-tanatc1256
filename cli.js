672898412c072b02eb1109d7c1bd3acebdc8acfc
root <root@PremsLaptop.localdomain>
2023-02-09 12:32:46 -0500

    a02_function completed

---
+++ cli.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+import fetch from 'node-fetch';
+import moment from 'moment-timezone';
+
+const args = minimist(process.argv.slice(2));
+if (args.h) {
+	 console.log("Usage: galosh.js [options] -[n|s] LATITUTE -[e|w] LONGITUTE -z TIME_ZONE");
+         console.log("    -h	          Show this help message and exit.");
+	 console.log("    -n, -s	  Latitute: N positive, S negative.");
+	 console.log("    -e, -w	  Longitute: E positive, W negative.");
+	 console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.");
+	 console.log("    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
+	 console.log("    -j            Echo pretty JSON from open-meteo API and exit.");
+	 process.exit(0);
+}
+
+const timezone = moment.tz.guess();
+
+const latitute;
+const longitute:
+
+if (args.n) {
+	latitute = args.n;
+}
+else if (args.s) {
+	lattitute = -1 * (args.s);
+}
+
+
+if (args.e) {
+	longitute = args.e;
+}
+else if (args.w) {
+	longitute = -1 * (args.w);
+}
+
+if (args.z) {
+	timezone = args.z;
+}
+
+const response = await fetch('https://ai.open-meteo.com/v1/forecast?latitute=' + latitute + '&longitute=' longitute);
+const data = await response.json();
+
+if (args.j) {
+	data = args.j;
+	process.exit(0);
+}
+
+const days = args.d
+
+if (days == 0) {
+	console.log("today.");
+}
+else if (days > 1) {
+	console =.log("in" + days + "days.");
+}
+else {
+	console.log("tomorrow.");
+}
+
